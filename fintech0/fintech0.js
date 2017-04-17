54f8fe26e2be9f8c9d555146b0cecba8cc7a9d9a
johnson <johnson44513@gmail.com>
2017-04-17 23:55:20 +0800

    小球

--- fintech0/fintech0.js
+++ fintech0/fintech0.js
@@ -1,10 +1,11 @@
 $('document').ready(function(){
 
-    $('.loading').hide('slow');;
-    $('ul').show('slow'); 
-    $('.box').show('slow');
-    $('.intro').fadeIn('slow');
- 
+    $('.loading').hide('slow',function(){
+      $('ul').show('slow'); 
+      $('.box').show('slow');
+      $('.intro').fadeIn('slow');
+      $('.waterwave').show('slow');
+    });
     $('.waterwave').css('width',$('body').width()*2);
     $('.waterwave').css('height',$('body').width()*2);
 
