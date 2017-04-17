dd1b61199ab5f6156be44bcf00eb1a3178147eec
johnson <johnson44513@gmail.com>
2017-04-17 23:59:12 +0800

    Q

--- fintech0/fintech0.js
+++ fintech0/fintech0.js
@@ -1,15 +1,14 @@
 $('document').ready(function(){
 
     $('.loading').hide('slow',function(){
-      $('ul').show('slow'); 
+      $('ul').show('slow');
+      $('li').show('slow');
       $('.box').show('slow');
       $('.intro').fadeIn('slow');
       $('.waterwave').show('slow');
     });
     $('.waterwave').css('width',$('body').width()*2);
     $('.waterwave').css('height',$('body').width()*2);
-
-
 
   
 }); 
